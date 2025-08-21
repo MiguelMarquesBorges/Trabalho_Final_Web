eed61896a977ef4bc5c6b0fd21669d5116c1a5f5
Jair <jair.ferronato@ifpr.edu.br>
2025-08-21 16:03:23 -0300

    Aula 21/08

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,42 @@
 @extends('layouts.main_layout')
 
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-sm-8">
+            <div class="card p-5">
+                <div class="text-center p-3">
+                    <img src="assets/images/logo.png" alt="Notes Logo">
+                </div>
+
+                <!-- Form -->
+                <div class="row justify-content-center">
+                    <div class="col-md-10 col-12">
+                        <form action="/loginSubmit" method="post">
+                        <div class="mb-3">
+                            <label for="text_username" class="form-label">Username</label>
+                            <input class="form-control bg-dark text-info" type="text" name="text_username" require>
+                        </div>
+                        <div class="mb-3">
+                            <label for="text_password" class="form-label">Password</label>
+                            <input class="form-control bg-dark text-info" type="text" name="text_password" require>
+                        </div>
+                        <div class="mb-3">
+                            <button class="btn btn-secondary w-100" type="submit">LOGIN</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="text-center text-secondary">
+                    <small>&copy; <?= date('Y') ?> Notes</small>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
 
