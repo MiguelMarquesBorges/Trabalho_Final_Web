093b7d3aaed95c1b6e9cf39cdb092973ef7adc81
Your Name <you@example.com>
2025-08-28 16:15:47 -0300

    Aula 28-08

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,8 +19,18 @@ class AuthController extends Controller
 
         $request->validate([
             'text_username' => 'required|email',
-            'text_password' => 'required',
-        ]);
+            'text_password' => 'required|min:6|max:12',
+        ],
+        [
+            //Mensagem para username
+            'text_username.required' => 'O campo de e-mail é obrigatório!',
+            'text_username.email' => 'O campo de e-mail deve conter um endereço válido!',
+
+            'text_password.required' => 'A senha é obrigatória!',
+            'text_password.min' => 'A senha deve ter pelo menos :min caracteres',
+            'text_password.max' => 'A senha deve ter no máximo :max caracteres',
+        ]
+    );
 
         $username = $request->input('text_username');
         $password = $request->input('text_password');
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,20 +13,36 @@
                 <!-- Form -->
                 <div class="row justify-content-center">
                     <div class="col-md-10 col-12">
-                        <form action="/loginSubmit" method="post">
+                        {{-- <form action="/loginSubmit" method="post"> --}}
+                        <form action="{{ route('login.submit') }}" method="POST" novalidate>
                         @csrf  
                         <div class="mb-3">
                             <label for="text_username" class="form-label">Username</label>
-                            <input class="form-control bg-dark text-info" type="text" name="text_username">
+                            <input class="form-control bg-dark text-info" type="text" name="text_username" value="{{ old('text_username') }}">
+                            @error('text_username')
+                                <div class="text-danger">{{ $message }} </div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label for="text_password" class="form-label">Password</label>
-                            <input class="form-control bg-dark text-info" type="text" name="text_password">
+                            <input class="form-control bg-dark text-info" type="password" name="text_password" value="{{ old('text_password') }}">
+                            @error('text_password')
+                                <div class="text-danger">{{ $message }} </div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <button class="btn btn-secondary w-100" type="submit">LOGIN</button>
                         </div>
                         </form>
+                        {{-- @if($errors->any())
+                            <div class="alert alert-danger mt-3">
+                                <ol class="m-0">
+                                    @foreach ($errors->all() as $error )
+                                        <li>{{ $error }} </li>
+                                    @endforeach
+                                </ol>
+                            </div>
+                        @endif --}}
                     </div>
                 </div>
 
