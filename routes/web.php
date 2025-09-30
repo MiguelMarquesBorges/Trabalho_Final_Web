0cdf9baae9c54fc063e6b877d88cdbde5796ef03
Lucas Canestraro <lucascanestrarooliveira@gmail.com>
2025-09-30 10:09:23 -0300

    Middlewares adicionados

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 
 
-Route::GET('/login', [AuthController::class, 'login'])->name('login');
 
-Route::POST('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('login.submit');
+Route::middleware([CheckIsLogged::class])->group(
 
-Route::GET('/logout', [AuthController::class, 'logout'])->name('logout');
+    function(){
+        Route::GET('/login', [AuthController::class, 'login'])->name('login');
+        Route::POST('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('login.submit');
+    }
+);
+
+Route::middleware([CheckIsNotLogged::class])->group(
+
+    function(){
+        Route::GET('/logout', [AuthController::class, 'logout'])->name('logout');
+    }
+);
 
 Route::GET('/home', [MainController::class, 'home'])->name('home');
 
