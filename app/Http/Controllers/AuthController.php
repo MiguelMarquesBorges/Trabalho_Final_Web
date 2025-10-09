fec839d05b430949b30cf4a4b5db72c81972e85d
Miguel Marques Borges <miguelmarquesborges21@gmail.com>
2025-10-09 17:19:05 -0300

    fhtgyjyjy

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
             ]
             ]);
         
-            return redirect()->route('home');
+            return redirect()->route('/home');
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use App\Http\Middleware\CheckIsNotLogged;
 
 Route::middleware([CheckIsNotLogged::class])->group(
 
-    function(){
+    function () {
         Route::GET('/login', [AuthController::class, 'login'])->name('login');
         Route::POST('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('login.submit');
-        
+
         Route::GET('/signin', [UserController::class, 'signIn'])->name('signin');
         Route::POST('/signInSubmit', [UserController::class, 'createUser'])->name('signin.submit');
 
@@ -24,26 +24,26 @@ Route::middleware([CheckIsNotLogged::class])->group(
 
 Route::middleware([CheckIsLogged::class])->group(
 
-    function(){
+    function () {
         Route::GET('/logout', [AuthController::class, 'logout'])->name('logout');
-        
+        Route::get('/times/create', [TimesController::class, 'create'])->name('times.create');
+        Route::post('/times/store', [TimesController::class, 'store'])->name('times.store');
+        Route::get('/times', [TimesController::class, 'index'])->name('times.list');
+        Route::get('/times/remove', [TimesController::class, 'removeView'])->name('times.remove');
+        Route::delete('/times/{id}', [TimesController::class, 'destroy'])->name('times.destroy');
+        Route::get('/times/{id}/edit', [TimesController::class, 'edit'])->name('times.edit');
+        Route::put('/times/{id}', [TimesController::class, 'update'])->name('times.update');
+
     }
 );
 
-Route::get('/times/create', [TimeController::class, 'create'])->name('times.create');
-Route::post('/times/store', [TimeController::class, 'store'])->name('times.store');
-Route::get('/times', [TimeController::class, 'index'])->name('times.list');
-Route::get('/times/remove', [TimeController::class, 'removeView'])->name('times.remove');
-Route::delete('/times/{id}', [TimeController::class, 'destroy'])->name('times.destroy');
-Route::get('/times/{id}/edit', [TimeController::class, 'edit'])->name('times.edit');
-Route::put('/times/{id}', [TimeController::class, 'update'])->name('times.update');
 
 Route::GET('/home', [MainController::class, 'home'])->name('home');
 
 Route::GET('/listAdmins', [UserController::class, 'list'])->name('listAdmins');
 Route::GET('/findAdmin/{user}', [UserController::class, 'find'])->name('findAdmin');
 
-Route::MATCH(['get','post', 'put'], '/updateAdmin/{user}', [UserController::class, 'update'])->name('updateAdmin');
+Route::MATCH(['get', 'post', 'put'], '/updateAdmin/{user}', [UserController::class, 'update'])->name('updateAdmin');
 Route::PUT('/updateUserSubmit', [UserController::class, 'updateUser'])->name('updateUser.submit');
 
 
